e28df45a97cd9594cde43821347d42313c44a0f3
Mr Den <denyszhyla2000@gmail.com>
2025-04-30 23:13:47 +0300

    Fixed database path lab3

--- lab3/src/assets/scripts/FunctionButtons.js
+++ lab3/src/assets/scripts/FunctionButtons.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 export const ShowCreateForm = (isFormVisible, setIsFormVisible) => {
     // setIsFormVisible(true);
     return 0;
-=======
-export const ShowCreateForm = (isFormVisible, setIsFormVisible) => {
-    // setIsFormVisible(true);
-    return 0;
->>>>>>> c5f44b3d83f19968b98d27be1ded60cfcabe06e9
 }
--- lab3/src/pages/MyInitiatives.js
+++ lab3/src/pages/MyInitiatives.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import InitiativesPage from "../components/InitiativesPage";
 
 export default function AvalibleInitiatives() {
@@ -7,13 +6,3 @@ export default function AvalibleInitiatives() {
         <InitiativesPage type="Мої ініціативи" />
     );
 }
-=======
-import InitiativesPage from "../components/InitiativesPage";
-
-export default function AvalibleInitiatives() {
-
-    return (
-        <InitiativesPage type="Мої ініціативи" />
-    );
-}
->>>>>>> c5f44b3d83f19968b98d27be1ded60cfcabe06e9
